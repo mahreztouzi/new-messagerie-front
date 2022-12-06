204a35c01645eb0811e6128a804d4b9a84f2b46f
mahrez <mahreztouzi@gmail.com>
2022-12-07 00:19:47 +0100

    modification de l interface messageie

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import onlineIcon from '../../icons/onlineIcon.png';
-import closeIcon from '../../icons/closeIcon.png';
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
 
-import './InfoBar.css';
+import "./InfoBar.css";
 
 const InfoBar = ({ room }) => (
   <div className="infoBar">
     <div className="leftInnerContainer">
-      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
-      <h3>{room}</h3>
+      {/* <img className="onlineIcon" src={onlineIcon} alt="online icon" /> */}
+      {/* <h3>{room}</h3> */}
+      <h3>Chat Room</h3>
     </div>
     <div className="rightInnerContainer">
-      <a href="/"><img src={closeIcon} alt="close icon" /></a>
+      <a href="/">
+        <img src={closeIcon} alt="close icon" />
+      </a>
     </div>
   </div>
 );
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import './Input.css';
+import { AiOutlineSend } from "react-icons/ai";
+import "./Input.css";
 
-const Input = ({ setMessage, sendMessage, message }) => (
-  <form className="form">
-    <input
-      className="input"
-      type="text"
-      placeholder="Type a message..."
-      value={message}
-      onChange={({ target: { value } }) => setMessage(value)}
-      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-    />
-    <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
-  </form>
-)
+const Inputs = ({ setMessage, sendMessage, message }) => {
+  return (
+    <form className="form">
+      <input
+        className="input"
+        type="text"
+        placeholder="Type a message..."
+        value={message}
+        onChange={({ target: { value } }) => setMessage(value)}
+        onKeyPress={(event) =>
+          event.key === "Enter" ? sendMessage(event) : null
+        }
+      />
 
-export default Input;
+      <button className="sendButton" onClick={(e) => sendMessage(e)}>
+        <AiOutlineSend style={{ fontSize: "20px", color: "#ccc" }} />
+      </button>
+    </form>
+  );
+};
+
+export default Inputs;
